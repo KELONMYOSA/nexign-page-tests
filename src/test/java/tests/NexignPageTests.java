c3d4032a9c74957bf4f35b4bf08fa842537f0186
Vladislav Samoilenko <79825223592@yandex.ru>
2022-03-21 20:40:57 +0300

    added attachment logo svg

--- src/test/java/tests/NexignPageTests.java
+++ src/test/java/tests/NexignPageTests.java
@@ -2,10 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import helpers.DriverUtils;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.net.URL;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -20,7 +24,13 @@ public class NexignPageTests extends TestBase {
     @Link(value = "Testing URL", url = "https://nexign.com/ru")
     void FillFormTest() {
         step("Открываем страницу 'https://nexign.com/ru'", () -> open("https://nexign.com/ru"));
-        step("Проверяем наличие изображения логотипа", () -> $(".header__logo").$("img").should(Condition.exist));
+        step("Проверяем наличие изображения логотипа", () -> {
+            $(".header__logo").$("img").should(Condition.exist);
+            URL logoUrl = new URL($(".header__logo").$("img").getAttribute("src"));
+            try (InputStream is = logoUrl.openConnection().getInputStream()) {
+                Allure.addAttachment("Logo SVG file","image/svg+xml", is, "svg");
+            }
+        });
         step("Проверяем видимость изображения логотипа", () -> $(".header__logo").$("img").shouldBe(Condition.visible));
     }
 
