117fe973d4045219ae2e230ec22b44f70bc1fd71
Vladislav Samoilenko <79825223592@yandex.ru>
2022-03-21 18:07:25 +0300

    added ExtendWith

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,10 +6,13 @@ import helpers.DriverSettings;
 import helpers.DriverUtils;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith({AllureJunit5.class})
 public class TestBase {
 
     @BeforeEach
