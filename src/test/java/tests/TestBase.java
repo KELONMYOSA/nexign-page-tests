be021f114c471b657e4c86ac0e1abbaeb9b0f520
Vladislav Samoilenko <79825223592@yandex.ru>
2022-03-21 18:17:43 +0300

    changed BeforeAll

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,20 +9,20 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DriverSettings.configure();
     }
 
     @AfterEach
-    void addAttachments() {
+    public void addAttachments() {
         AllureAttachments.addScreenshotAs("Last screenshot");
         AllureAttachments.addPageSource();
         AllureAttachments.addBrowserConsoleLogs();
